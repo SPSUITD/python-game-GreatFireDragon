e7fc51413ad441af13f6238371ae0c69f7c94c1f
greatfiredragon <kulmike2002@gmail.com>
2023-02-22 01:55:33 +0300

    Rules button added

--- Menu.py
+++ Menu.py
@@ -7,6 +7,7 @@ data = json.load(f)
 
 from MainGame import MainGame
 from Settings import Settings
+from Rules import Rules
 from static.constants import *
 from GeneralModule import cursor_coordinates
 
@@ -32,6 +33,10 @@ def create_buttons(self):
     start_button = arcade.gui.UIFlatButton(text="Start Game", width=200, style=MENU_STYLE)
     self.v_box.add(start_button.with_space_around(bottom=20))
     start_button.on_click = self.on_click_start
+
+    rules_button = arcade.gui.UIFlatButton(text="Rules", width=200, style=MENU_STYLE)
+    self.v_box.add(rules_button.with_space_around(bottom=20))
+    rules_button.on_click = self.on_click_rules
 
     settings_button = arcade.gui.UIFlatButton(text="Settings", width=200, style=MENU_STYLE)
     self.v_box.add(settings_button.with_space_around(bottom=20))
@@ -82,6 +87,11 @@ class Menu(arcade.View):
         view = Settings()
         self.window.show_view(view)
 
+    def on_click_rules(self, event):
+        self.manager.clear()
+        view = Rules()
+        self.window.show_view(view)
+
 
     def on_draw(self):
         self.clear()
@@ -111,7 +121,7 @@ class Menu(arcade.View):
 
     def on_key_press(self, symbol, modifiers):
         on_key_basic_press(self, symbol, modifiers)
-        
+
 
         # if symbol == 65480:
         #     self.manager.clear()
---
+++ Rules.py
@@ -0,0 +1,104 @@
+# modules
+import arcade
+import arcade.gui
+import json
+f = open("static/controls.json")
+data = json.load(f)
+
+# from Menu import Menu
+import Menu
+from static.constants import *
+from GeneralModule import cursor_coordinates
+
+from basicControls import on_mouse_basic_press, on_mouse_basic_release, on_mouse_basic_motion, on_mouse_basic_enter, on_mouse_basic_leave, on_key_basic_press, on_key_basic_release
+    
+
+
+
+
+def create_buttons(self):
+    self.v_box = arcade.gui.UIBoxLayout()
+
+    get_back_button = arcade.gui.UIFlatButton(text="← Get Back", width=200, style=MENU_STYLE)
+    self.v_box.add(get_back_button.with_space_around(bottom=20))
+    get_back_button.on_click = self.on_click_get_back
+
+
+    self.manager.add(
+        arcade.gui.UIAnchorWidget(anchor_x="center_x", anchor_y="bottom", child=self.v_box)
+        )
+
+#  MENU -----------------------------------------------------------
+class Rules(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+        # CURSOR
+        self.window.set_mouse_visible(False)
+        self.cursor_sprite = arcade.Sprite("images/HANDS_CURSOR_1.png", 1)
+
+        #  MANAGER
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+        arcade.set_background_color(BG_MENU)        # ATAVISM
+
+        # GRADIENT BG
+        self.shapes = arcade.ShapeElementList()
+        rect = arcade.create_rectangle_filled_with_colors(GRADIENT_POINTS, GRADIENT_COLOR)
+        self.shapes.append(rect)
+        # GRADIENT BG
+
+        create_buttons(self)
+        
+
+
+
+    def on_click_get_back(self, event):
+        self.manager.clear()
+        view = Menu.Menu()
+        self.window.show_view(view)
+
+
+    def on_draw(self):
+        self.clear()
+        self.shapes.draw()          # Gradient BG
+
+
+        left, screen_width, bottom, screen_height = self.window.get_viewport()
+        arcade.draw_text("Congue inceptos orci quam mauris per vitae maecenas.",
+                         screen_width // 2, screen_height // 2 + 60, font_name=FONT, font_size=SETTIGNS_FONT_SIZE, anchor_x="center")
+        arcade.draw_text("Dapibus sociosqu tristique hymenaeos bibendum commodo",
+                         screen_width // 2, screen_height // 2 + 20, font_name=FONT, font_size=SETTIGNS_FONT_SIZE, anchor_x="center")
+        arcade.draw_text("semper nunc cum accumsan velit class commodo.",
+                         screen_width // 2, screen_height // 2 - 20, font_name=FONT, font_size=SETTIGNS_FONT_SIZE, anchor_x="center")
+        arcade.draw_text("Est mollis cum vulputate nulla ad Gravida in vivamus.",
+                         screen_width // 2, screen_height // 2 - 60, font_name=FONT, font_size=SETTIGNS_FONT_SIZE, anchor_x="center")
+
+
+
+        self.manager.draw()         # Buttons (menu)
+        self.cursor_sprite.draw()   # должен быть последним!
+
+   
+    # CONTROLS -----------------------------------------------------
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        on_mouse_basic_press(self, x, y, button, key_modifiers)
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        on_mouse_basic_release(self, x, y, button, modifiers)
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        on_mouse_basic_motion(self, x, y, dx, dy)
+
+    def on_mouse_enter(self, x, y):
+        on_mouse_basic_enter(self, x, y)
+
+    def on_mouse_leave(self, x, y):
+        on_mouse_basic_leave(self, x, y)
+
+    def on_key_press(self, symbol, modifiers):
+        on_key_basic_press(self, symbol, modifiers)
+
+    def on_key_release(self, symbol, modifiers):
+       on_key_basic_release(self, symbol, modifiers)
+    # CONTROLS -----------------------------------------------------
