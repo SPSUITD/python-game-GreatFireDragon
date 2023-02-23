72e881539ec519fdaf7c8a362d9ab892f6d17f42
greatfiredragon <kulmike2002@gmail.com>
2023-02-23 19:06:32 +0300

    may be will scrap the map idea. Or rethink at least.

--- MainGame/_setup.py
+++ MainGame/_setup.py
@@ -7,19 +7,18 @@ from ._controls import *
 
 
 def setup(self):
-    # Read in the tiled map
-    TILE_SCALING = 1
 
-    # Name of map file to load
-    map_name = "maps/introduction.json"
-    layer_options = {
-            "Tile Layer 1": {
-                "use_spatial_hash": True,
-            },
-        }
+    
 
-    self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
-    self.scene = arcade.Scene.from_tilemap(self.tile_map)
+    # TILE_SCALING = 1
+    # map_name = "maps/introduction.json"
+    # layer_options = {
+    #         "Tile Layer 1": {
+    #             "use_spatial_hash": True,
+    #         },
+    #     }
+    # self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+    # self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
     # Set the background color
     if self.tile_map.background_color:
