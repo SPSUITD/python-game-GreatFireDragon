9dea7d18e000e991049ea3d6226ba0e9aac46cd9
greatfiredragon <kulmike2002@gmail.com>
2023-03-09 12:54:35 +0300

    build tests

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from MainGame import MainGame
 from Menu import Menu
 from constants import *
 
-from pathlib import Path
-path_to_dat = Path(__file__).resolve().with_name("controls.json")
 
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=data["fullscreen"])
